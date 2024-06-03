386af745497f2329fe4a1ac736e05f9d210244ea
sophture <sophia.balboni@sptech.school>
2024-06-03 17:20:15 -0300

    estilização do fórúm

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -19,15 +19,13 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
+                        res.json({
+                            id: resultadoAutenticar[0].idUsuario,
+                            email: resultadoAutenticar[0].email,
+                            nome: resultadoAutenticar[0].nome,
+                            senha: resultadoAutenticar[0].senha,
+                        });
 
-                        
-                            res.json({
-                                id: resultadoAutenticar[0].idUsuario,
-                                email: resultadoAutenticar[0].email,
-                                nome: resultadoAutenticar[0].nome,
-                                senha: resultadoAutenticar[0].senha,
-                            });
-                        
 
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
